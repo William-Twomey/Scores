a0fc4ebb7aed5a862f300eb49778ca6e70a76a24
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-10-05 16:08:04 +0100

    Add project files.

---
+++ Scores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            var date = DateTime.Today.ToShortDateString();
+            var userName = Console.ReadLine();
+            var message = $"Welcome back {userName} today is the {date}";
+            Console.WriteLine(message);
+
+            string path = @"C:\Users\will2\source\repos\Scores\Scores\studentscores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double totalScore = 0.0;
+
+            Console.WriteLine("\nStudent Score: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                totalScore += score;
+            }
+
+            double avgScore = totalScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " students score. \tAverage Score: " + avgScore);
+
+            Console.WriteLine("\n\n\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
+
+
